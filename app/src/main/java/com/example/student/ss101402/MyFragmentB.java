fab0491bd8b8ea0bec474464e3c993ed69e744c6
doris <doris@yahoo.com.tw>
2016-10-14 10:09:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/ss101402/MyFragmentB.java
@@ -0,0 +1,20 @@
+package com.example.student.ss101402;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by student on 2016/10/14.
+ */
+
+public class MyFragmentB extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b, null);
+    }
+}
